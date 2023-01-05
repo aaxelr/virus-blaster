e3e7ebd4b681d5f629369e2d7b303ef89bad6cff
aaxelr <antonio.axelsson@gmail.com>
2023-01-05 20:03:21 +0100

    add placeholder sounds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rusty_audio::Audio;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "src/sounds/explode.wav");
+    audio.add("lose", "src/sounds/lose.wav");
+    audio.add("move", "src/sounds/move.wav");
+    audio.add("pew", "src/sounds/pew.wav");
+    audio.add("startup", "src/sounds/startup.wav");
+    audio.add("win", "src/sounds/win.wav");
+    audio.play("startup");
+
+    // Clean up: block until all audio is done playing.
+    audio.wait();
+    Ok(())
 }
